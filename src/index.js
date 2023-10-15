4db8527be73410360d09eef9a352454d11cf64eb
Melanie Rodas <mrodasl@uni.pe>
2023-10-15 15:30:32 -0500

    Add package.json and install dependecies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import ProductRouter from './router/product.routes.js';
-import CartRouter from './router/carts.routes.js';
 
 const app = express();
 const PORT = 8080;
@@ -8,8 +6,8 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/products', ProductRouter);
-app.use('/api/cart', CartRouter);
+/*app.use('/api/products', ProductRouter);
+app.use('/api/cart', CartRouter);*/
 
 app.listen(PORT, () => {
   console.log(`Servidor Express Puerto ${PORT}`);
